e5e982bb1ae372015aaa66da60c291d5b2edc95a
Marcus Daly <mrd109@case.edu>
2023-06-10 01:10:16 -0400

    escape home in bf alias

--- shell_setup_base.sh
+++ shell_setup_base.sh
@@ -5,7 +5,7 @@ export PATH="/opt/homebrew/opt/node@16/bin:$PATH"
 export CLICOLOR=1
 export LSCOLORS=ExFxBxDxCxegedabagacad
 
-alias bf="source $HOME/dotfiles/scripts/black_flake8.sh"
+alias bf="source \$HOME/dotfiles/scripts/black_flake8.sh"
 alias sa="source venv/bin/activate"
 alias python3="python"
 alias make-venv="deactivate && conda activate py310 && python -m venv venv && conda deactivate && source venv/bin/activate && python -m pip install --upgrade pip wheel pip-tools"
